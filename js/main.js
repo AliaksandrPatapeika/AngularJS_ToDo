4ce44311ffd3248bdfb611f53784fb4d8633a18b
Aliaksandr Patapeika <Aliaksandr_Patapeika@epam.com>
2019-03-19 18:07:08 +0300

    filter buttons class names refactoring

--- js/main.js
+++ js/main.js
@@ -1,20 +1,44 @@
+const model = {
+  // хранилище для всех заданий
+  taskList: [
+    {text: 'learn AngularJS', done: true, important: false},
+    {text: 'build an AngularJS app', done: false, important: false},
+    {text: 'Other task', done: false, important: true}
+  ],
+  filterButtonsList: [
+    {name: 'all', label: 'All'},
+    {name: 'active', label: 'Active'},
+    {name: 'done', label: 'Done'}
+  ],
+  filterTasks: 'all',
+  searchTaskInputText: ''
+};
+
 // создание модуля
 angular.module('todoListApp', [])
 // контроллер, где будут обрабатываться все данные. $scope - хранилище
     .controller('todoListCtrl', function ($scope) {
-          // хранилище для всех заданий
-          $scope.taskList = [
-            {text: 'learn AngularJS', done: true, important: false},
-            {text: 'build an AngularJS app', done: false, important: false},
-            {text: 'Other task', done: false, important: true}
-          ];
 
-          $scope.addTaskInputText = '';
-          $scope.searchTaskInputText = '';
-          $scope.filterTasks = 'all';
+          $scope.data = model;
+
+          // вычисляемое свойство оставшихся для выполнения задач
+          $scope.remainingTasks = function () {
+            return $scope.data.taskList.filter((task) => !task.done).length;
+          };
+
+          $scope.archiveCompletedTasks = function () {
+            const oldTaskList = $scope.data.taskList;
+            $scope.data.taskList = [];
+            angular.forEach(oldTaskList, function (task) {
+              if (!task.done) {
+                $scope.data.taskList.push(task);
+              }
+            });
+            console.log('Old task list: ', oldTaskList);
+          };
 
           $scope.onFilterChange = (filterName) => {
-            $scope.filterTasks = filterName;
+            $scope.data.filterTasks = filterName;
           };
 
           $scope.searchTasks = function (taskList, searchText) {
@@ -26,30 +50,6 @@ angular.module('todoListApp', [])
               return taskList;
             }
           };
-
-          $scope.filterButtonsList = [
-            {
-              name: 'all',
-              label: 'All',
-              classNames: function () {
-                return 'btn ' + ((this.name === $scope.filterTasks) ? 'btn-filter-active' : 'btn-filter');
-              },
-            },
-            {
-              name: 'active',
-              label: 'Active',
-              classNames: function () {
-                return 'btn ' + ((this.name === $scope.filterTasks) ? 'btn-filter-active' : 'btn-filter');
-              },
-            },
-            {
-              name: 'done',
-              label: 'Done',
-              classNames: function () {
-                return 'btn ' + ((this.name === $scope.filterTasks) ? 'btn-filter-active' : 'btn-filter');
-              },
-            }
-          ];
 
           $scope.taskStatusFilter = function (taskList, filterName) {
             switch (filterName) {
@@ -66,42 +66,32 @@ angular.module('todoListApp', [])
           };
 
           $scope.visibleTasks = function () {
-            return $scope.searchTasks($scope.taskStatusFilter($scope.taskList, $scope.filterTasks), $scope.searchTaskInputText);
+            return $scope.searchTasks($scope.taskStatusFilter($scope.data.taskList, $scope.data.filterTasks), $scope.data.searchTaskInputText);
+          };
+
+
+          $scope.importantTask = function (task) {
+            task.important = !task.important;
+          };
+
+
+          $scope.deleteTask = function (task) {
+            const index = $scope.data.taskList.indexOf(task);
+            $scope.data.taskList.splice(index, 1)
           };
 
           $scope.addTask = function () {
             // не добавлять пустые задания
             if ($scope.addTaskInputText) {
-              $scope.taskList.push({text: $scope.addTaskInputText, done: false, important: false});
+              $scope.data.taskList.push({
+                text: $scope.addTaskInputText,
+                done: false,
+                important: false
+              });
               $scope.addTaskInputText = '';
             } else {
               console.log('Empty input');
             }
-          };
-
-          // вычисляемое свойство оставшихся для выполнения задач
-          $scope.remaining = function () {
-            return $scope.taskList.filter((task) => !task.done).length;
-          };
-
-          $scope.archive = function () {
-            const oldTaskList = $scope.taskList;
-            $scope.taskList = [];
-            angular.forEach(oldTaskList, function (task) {
-              if (!task.done) {
-                $scope.taskList.push(task);
-              }
-            });
-            console.log('Old task list: ', oldTaskList);
-          };
-
-          $scope.deleteTask = function (task) {
-            const index = $scope.taskList.indexOf(task);
-            $scope.taskList.splice(index, 1)
-          };
-
-          $scope.importantTask = function (task) {
-            task.important = !task.important;
           };
 
         }
