4ccf6de30862fce5b24748862c67e838dec84e66
Aliaksandr Patapeika <Aliaksandr_Patapeika@epam.com>
2019-03-20 18:29:53 +0300

    refactoring from work (20.03.2019)

--- js/main.js
+++ js/main.js
@@ -37,43 +37,9 @@ angular.module('todoListApp', [])
             console.log('Old task list: ', oldTaskList);
           };
 
-          $scope.onFilterChange = (filterName) => {
-            $scope.data.filterTasks = filterName;
-          };
-
-          $scope.searchTasks = function (taskList, searchText) {
-            if (searchText) {
-              return taskList.filter((task) => {
-                return task.text.toLowerCase().indexOf(searchText.toLowerCase()) > -1;
-              });
-            } else {
-              return taskList;
-            }
-          };
-
-          $scope.taskStatusFilter = function (taskList, filterName) {
-            switch (filterName) {
-              case 'all':
-                return taskList;
-              case 'active':
-                return taskList.filter((task) => (!task.done));
-              case 'done':
-                return taskList.filter((task) => task.done);
-              default:
-                console.log('default for debug', filterName);
-                return taskList;
-            }
-          };
-
-          $scope.visibleTasks = function () {
-            return $scope.searchTasks($scope.taskStatusFilter($scope.data.taskList, $scope.data.filterTasks), $scope.data.searchTaskInputText);
-          };
-
-
           $scope.importantTask = function (task) {
             task.important = !task.important;
           };
-
 
           $scope.deleteTask = function (task) {
             const index = $scope.data.taskList.indexOf(task);
@@ -92,7 +58,22 @@ angular.module('todoListApp', [])
             } else {
               console.log('Empty input');
             }
-          };
+          }
 
         }
-    );
+    )
+    .filter('myFilter', function () {
+      return function (taskList, filterName) {
+        switch (filterName) {
+          case 'all':
+            return taskList;
+          case 'active':
+            return taskList.filter((task) => (!task.done));
+          case 'done':
+            return taskList.filter((task) => task.done);
+          default:
+            console.log('default for debug', filterName);
+            return taskList;
+        }
+      }
+    });
